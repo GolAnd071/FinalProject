cad0152263d1843cc086a3423cfe9e7d6fc038c9
Dribas Roman <42975657+mrdrrmn@users.noreply.github.com>
2021-12-15 21:04:18 +0300

    Added BrokenLine class.

--- Algorithm/choosing_coastline.py
+++ Algorithm/choosing_coastline.py
@@ -1,11 +1,12 @@
 # TODO: load pix massive from another module # We don't need to load it, we will create an object of this class in module, where pix massive is - GolAnd071
 import numpy as np
 
+
 def checking_if_border(neighbor_area: np.array):
     '''Cheking if there are water bordering pixels in area'''
-    coords = [(0,1), (0,1), (2, 1), (1, 2)] # [(0, 1), (1, 0), (1, 2), (2, 1)] - GolAnd071
+    coords = [(0, 1), (0, 1), (2, 1), (1, 2)]  # [(0, 1), (1, 0), (1, 2), (2, 1)] - GolAnd071
     for c in coords:
-        if neighbor_area[c] == 1: #cheking if pixel is water pixel
+        if neighbor_area[c] == 1:  # cheking if pixel is water pixel
             return True
     return False
 
@@ -24,15 +25,18 @@ class Coastline:
 
     def get_next_coords(self) -> tuple:
         # TODO: checking if next_coords go beyond the pix
-        x,y = self.coords[-1]
+        x, y = self.coords[-1]
         for i in range(3):
             for j in range(3):
-                if (i, j) not in (self.coords[-2], self.coords[-1]) and next_neighbor_area[i, j] == 0 and checking_if_border(i, j):
-                    return y+i, x+j
+                if (i, j) not in (self.coords[-2], self.coords[-1]) and next_neighbor_area[
+                    i, j] == 0 and checking_if_border(i, j):
+                    return y + i, x + j
 
-    #TODO: add function choose_first_coords or choose_first_point
+    # TODO: add function choose_first_coords or choose_first_point
+
 
 coastline = Coastline()
+
 
 # TODO: get all coastline.coords using cycle
 
@@ -43,3 +47,44 @@ coastline = Coastline()
 # pix = [[0, 1, 0, 0, 1],
 #        [0, 1, 1, 0, 0
 #        []]
+
+
+class BrokenLine:
+    step = 0
+    start_point = []
+    cords = []
+
+    def __init__(self, step, start_point, cords):
+        self.step = step
+        self.start_point = start_point
+        self.cords = cords
+
+    def create_line(self, cords):
+        """
+        Creates a list of coordinates of vertices of broken line of coastline
+        @param cords: list of coastline coordinates
+        """
+        def add_new_cord(line: BrokenLine, previous_cord, start_cord, coords):
+            epsilon = 1000000
+            end_cord = [0, 0]
+            for c in coords:
+                d = ((start_cord[0] - c[0]) ** 2 + (start_cord[1] - c[1]) ** 2) ** 0.5
+                delta = int(d) - line.step
+                if epsilon ** 2 > delta ** 2 and previous_cord != [c[0], c[1]]:
+                    epsilon = delta
+                    end_cord = [c[0], c[1]]
+            line.cords.append(end_cord)
+            return start_cord, end_cord
+
+        prev = self.start_point
+        curr = self.start_point
+        for _ in range(2):
+            prev, curr = add_new_cord(self, prev, curr, cords)
+        while ((curr[0] - self.start_point[0]) ** 2 + (curr[1] - self.start_point[1]) ** 2) >= self.step ** 2:
+            prev, curr = add_new_cord(self, prev, curr, cords)
+
+    def get_length(self):
+        """
+        Returns length of broken line
+        """
+        return self.step * len(self.cords)
