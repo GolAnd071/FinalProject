38bea70142233d7d4858dccc640450e63b0a12e9
GrishkaYesenin <grigorevaesenia@gmail.com>
2021-12-19 13:55:00 +0300

    Reformating class Coastline

--- Algorithm/choosing_coastline.py
+++ Algorithm/choosing_coastline.py
@@ -1,81 +1,116 @@
-# TODO: load pix massive from another module # We don't need to load it, we will create an object of this class in module, where pix massive is - GolAnd071
-
-
-def check_if_border(neighbor_area):
-    '''Cheking if there are water bordering pixels in area'''
-    coords = [[0, 1], [1, 0], [1, 2], [2, 1]]
-    for c in coords:
-        if neighbor_area[c[0]][c[1]] == 1:  # cheking if pixel is water pixel
-            return True
-    return False
-
-
-def check_if_reached_array_frames(coords, array_size):
+def check_if_go_out_of_array(coords, array_size):
     '''Cheking if we've reached array frames'''
+
     for c in coords:
-        if c >= array_size - 1:
+        if c > array_size - 1 or c < 0:
             return True
     return False
 
 
 class Coastline:
+    counterclockwise_bypass = [(1, 0), (0, 1), (-1, 0), (0, -1)]
+    clockwise_bypass = [(0, 1), (1, 0), (0, -1), (-1, 0)]
+    bypasses = counterclockwise_bypass, clockwise_bypass
+    # TODO: check if algo depends on bypass direction (shifts order)
+    # TODO: replace tuples array by numpy array
+
     def __init__(self, start_point, data_size, data):
         self.coords = [start_point]
         self.start_point = start_point
         self.data = data
         self.data_size = data_size
 
-    def get_next_neighborhood_area(self, center: tuple):
+    def check_if_point_is_border(self, coord: tuple):
+        '''Cheking if there are water bordering pixels in area'''
         data = self.data
-        y, x = center
-        neighborhood_area = [[data[y + 1][x - 1], data[y + 1][x], data[y + 1][x + 1]],
-                             [data[y][x - 1], data[y][x], data[y][x + 1]],
-                             [data[y - 1][x - 1], data[y - 1][x], data[y + 1][x + 1]]]
-        return neighborhood_area
+        y, x = coord
+        for i in range(-1, 2):
+            for j in range(-1, 2):
+                neighbor_coord = y + i, x + j
+                if not check_if_go_out_of_array(neighbor_coord, self.data_size) and neighbor_coord != coord:
+                    if data[y + i][x + j] == 1:
+                        return True
+        return False
 
-    def get_new_coords(self) -> tuple:
+    def check_if_came_back(self, new_coord):
+        if len(self.coords) <= 1:
+            if new_coord == self.coords[-1]:
+                return True
+            else:
+                return False
+        elif new_coord in (self.coords[-2], self.coords[-1]):
+            return True
+        else:
+            return False
+
+    def get_new_coords(self, bypass) -> tuple:
         y, x = self.coords[-1]
-        next_neighbor_area = self.get_next_neighborhood_area(center=(y, x))
-        for i in range(3):
-            for j in range(3):
-                if (i, j) not in (self.coords[-1][0],
-                                  self.coords[-1][1]) and \
-                        next_neighbor_area[i][j] == 0 and \
-                        check_if_border(next_neighbor_area):
-                    return y + i, x + j
+        for i, j in bypass:
+            new_coord = y + i, x + j
+            if not check_if_go_out_of_array(new_coord, self.data_size):
+                if not self.check_if_came_back(new_coord) and \
+                        self.data[y + i][x + j] == 0 and \
+                        self.check_if_point_is_border(coord=new_coord):
+                    # print(f'new_coord: {new_coord}')
+                    return new_coord
+        return self.start_point
 
     def create_line(self):
         """
         Creates a list of coordinates of coastline
         """
-        while not check_if_reached_array_frames(self.coords[-1], self.data_size) or \
-                self.coords[-1] == self.start_point:
-            self.coords += [self.get_new_coords()]
+
+        print(f'I am on 65 line: self.coords {self.coords}')
+        i=1
+        while self.get_new_coords(bypass=self.bypasses[0]) != self.start_point:
+            self.coords += [self.get_new_coords(bypass=self.bypasses[0])]
+            # print(f'Number of itter: {i}, прибавляем координаты: {self.coords[-1]}, bp: против час')
+            i+=1
+        # self.coords += [self.get_new_coords(bypass=self.bypasses[1])]
+        self.coords.reverse()
+        print(f'I am on 72 line: self.coords {self.coords}')
+        while self.get_new_coords(bypass=self.bypasses[1]) != self.start_point:
+            self.coords += [self.get_new_coords(bypass=self.bypasses[1])]
+            # print(f'Number of itter: {i}, последняя координата: {self.coords[-1]},  bp: по час')
+            i += 1
 
 
 class BrokenLine:
-    def __init__(self, step, cords):
+    step = 0
+    start_point = []
+    cords = []
+
+    def __init__(self, step, start_point, cords):
         self.step = step
-        self.start_point = cords[0]
-        self.vertices = [self.start_point]
+        self.start_point = start_point
         self.cords = cords
 
-    def create_line(self):
+    def create_line(self, cords):
         """
         Creates a list of coordinates of vertices of broken line of coastline
+        @param cords: list of coastline coordinates
         """
-        cur = self.start_point
-        for i in range(len(self.cords)):
-            dists = [((cur[0] - self.cords[i][0] - 0.5) ** 2 + (cur[0] - self.cords[i][0] - 0.5) ** 2) ** 0.5,
-                     ((cur[0] - self.cords[i][0] - 0.5) ** 2 + (cur[0] - self.cords[i][0] + 0.5) ** 2) ** 0.5,
-                     ((cur[0] - self.cords[i][0] + 0.5) ** 2 + (cur[0] - self.cords[i][0] - 0.5) ** 2) ** 0.5,
-                     ((cur[0] - self.cords[i][0] + 0.5) ** 2 + (cur[0] - self.cords[i][0] + 0.5) ** 2) ** 0.5]
-            if min(dists) <= self.step <= max(dists):
-                self.vertices.append(self.cords[i])
-                cur = self.cords[i]
+        def add_new_cord(line: BrokenLine, previous_cord, start_cord, coords):
+            epsilon = 1000000
+            end_cord = [0, 0]
+            for c in coords:
+                d = ((start_cord[0] - c[0]) ** 2 + (start_cord[1] - c[1]) ** 2) ** 0.5
+                delta = int(d) - line.step
+                if epsilon ** 2 > delta ** 2 and previous_cord != [c[0], c[1]]:
+                    epsilon = delta
+                    end_cord = [c[0], c[1]]
+            line.cords.append(end_cord)
+            return start_cord, end_cord
+
+        prev = self.start_point
+        curr = self.start_point
+        for _ in range(2):
+            prev, curr = add_new_cord(self, prev, curr, cords)
+        while ((curr[0] - self.start_point[0]) ** 2 + (curr[1] - self.start_point[1]) ** 2) >= self.step ** 2:
+            prev, curr = add_new_cord(self, prev, curr, cords)
 
     def get_length(self):
         """
         Returns length of broken line
         """
-        return self.step * len(self.vertices)
+        return self.step * len(self.cords)
--- Algorithm/test_choosing_coastline.py
+++ Algorithm/test_choosing_coastline.py
@@ -3,64 +3,70 @@ from random import randint
 import choosing_coastline as cc
 
 
-def make_array(n):
-    arr = []
-    for _ in range(n):
-        r = []
-        for __ in range(n):
-            r.append(randint(0, 1))
-        arr.append(r)
-    return arr
-
-
 class Test_choosing_coastline(unittest.TestCase):
     def setUp(self):
+        def make_array(n):
+            arr = []
+            for _ in range(n):
+                r = []
+                for __ in range(n):
+                    r.append(randint(0, 1))
+                arr.append(r)
+            arr[0][0] = 0
+            print(arr)
+            return arr
+
         self.maps = []
         self.coastlines = []
         self.brokenlines = []
         for i in range(20):
             self.maps.append(make_array(3))
-            cstln = cc.Coastline((0, 0), 0, self.maps[i])
-            print(i, ' Coastline created')
+            cstln = cc.Coastline((0, 0), 3, self.maps[i])
+            print(i, ' Coastline created\n')
+            print(i, ' Coastline line started crating')
             cstln.create_line()
-            print(i, ' Coastline line created')
+            print(f'cstln.coords:{cstln.coords}')
+            print(i, ' Coastline line created\n')
+            print(i, ' BrokenLine started crating')
             brln = cc.BrokenLine(randint(1, 3), cstln.coords)
-            print(i, ' BrokenLine created')
+            print(i, ' BrokenLine created\n')
+            print(i + 1, ' Coastline started crating')
             self.coastlines.append(cstln)
             self.brokenlines.append(brln)
-        for i in range(10):
+        for i in range(20):
             self.maps.append(make_array(10))
-            cstln = cc.Coastline((0, 0), 0, self.maps[i + 20])
-            print(i + 20, ' Coastline created')
+            cstln = cc.Coastline((0, 0), 10, self.maps[i])
+            print(i + 20, ' Coastline created\n')
+            print(i + 20, ' Coastline line started crating')
             cstln.create_line()
-            print(i + 20, ' Coastline line created')
+            print(i + 20, ' Coastline line created\n')
+            print(i + 20, ' BrokenLine started crating')
             brln = cc.BrokenLine(randint(1, 10), cstln.coords)
-            print(i + 20, ' BrokenLine created')
+            print(i + 20, ' BrokenLine created\n')
+            print(i + 21, ' Coastline started crating')
             self.coastlines.append(cstln)
             self.brokenlines.append(brln)
 
     def test_create_line(self):
         for i in range(len(self.brokenlines)):
             self.brokenlines[i].create_line()
-            print(i, ' BrokenLine line created')
+            print(i, ' BrokenLine line created\n')
 
     def test_get_lenght(self):
         for i in range(len(self.brokenlines)):
             self.brokenlines[i].get_length()
-            print(i, ' BrokenLine lenght had got')
+            print(i, ' BrokenLine lenght had got\n')
 
 
 if __name__ == '__main__':
     unittest.main()
 
-
-
-#def main():
+# def main():
 #    cstln = [(0, 5), (1, 4), (2, 4), (3, 3), (4, 2), (4, 1), (5, 2), (5, 3), (6, 4), (7, 4)]
 #    line = BrokenLine(int(input()), cstln)
 #    line.create_line()
 #    print(line.vertices)
 
 
-#if __name__ == '__main__':
+# if __name__ == '__main__':
 #    main()
